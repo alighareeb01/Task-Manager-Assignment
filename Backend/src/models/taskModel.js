6775894151e8d5c261d66dbf20290f7d48d6b860
alighareeb01 <ghareeb874@gmail.com>
2026-07-26 23:24:34 +0300

    fix something

--- Backend/src/models/taskModel.js
+++ Backend/src/models/taskModel.js
@@ -5,7 +5,6 @@ const taskSchema = new mongoose.Schema(
       type: String,
       required: [true, "Task title is required"],
       maxlength: 100,
-      unique: true,
     },
     description: {
       type: String,
